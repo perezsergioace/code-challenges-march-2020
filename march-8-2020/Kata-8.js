0ce874d5b39b052a917f5a0caa695cc6fc056a04
Sergio Perez <perez.sergio.ace@gmail.com>
2020-03-08 20:48:50 -0400

    Completed Kata 8

---
+++ march-8-2020/Kata-8.js
@@ -0,0 +1,28 @@
+// Write function bmi that calculates body mass index (bmi = weight / height ^ 2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+	if (weight / (height * height) <= 18.5) {
+		return 'Underweight'
+	}
+	if (weight / (height * height) <= 25) {
+		return 'Normal'
+	}
+	if (weight / (height * height) <= 30) {
+		return 'Overweight'
+	}
+	if (weight / (height * height) > 30) {
+		return 'Obese'
+	}
+}
+
+console.log(bmi(80, 1.8))
+
+// Completed
